0df7414c3c08742a791d7845dfcbec77325eaf3d
Truong Son <truongsonpt.80@gmail.com>
2025-02-20 17:19:35 +0700

    update

--- public/plugin.js
+++ public/plugin.js
@@ -1,39 +1,38 @@
-const elementPlugin = document.querySelector("#plugin");
-const iconElement = document.createElement("div");
-const contentElement = document.createElement("div");
+const elementPlugin = document.querySelector('#plugin');
+const iconElement = document.createElement('div');
+const contentElement = document.createElement('div');
 const iframeElement = document.createElement('iframe');
 
-
-function handleCLickEventIconPluginApp (e) {
+function handleCLickEventIconPluginApp(e) {
     let dataShow = JSON.parse(e.currentTarget.dataset.click_show);
     e.currentTarget.dataset.click_show = true;
-    elementPlugin.classList.toggle("show-plugin-content")
-    contentElement.classList.toggle("content-show");
+    elementPlugin.classList.toggle('show-plugin-content');
+    contentElement.classList.toggle('content-show');
 
-    if(!dataShow) {
+    if (!dataShow) {
         contentElement.appendChild(iframeElement);
-        iframeElement.src ="http://localhost:3000";
+        iframeElement.src = 'https://ai-chat-one-nu.vercel.app';
         dataShow = !dataShow;
     }
 }
 
-function startPluginApp(){
-    iconElement.className= "icon-plugin"
+function startPluginApp() {
+    iconElement.className = 'icon-plugin';
     iconElement.dataset.click_show = false;
-    iconElement.addEventListener("click", handleCLickEventIconPluginApp)
-    iframeElement.allow = "microphone; fullscreen";
+    iconElement.addEventListener('click', handleCLickEventIconPluginApp);
+    iframeElement.allow = 'microphone; fullscreen';
     iconElement.innerHTML = `
         <div style="display: flex; align-items:center; gap: 6px; padding: 4px 6px; color: #fff;">
             <span>Robox AI</span>
             <img src="https://cdn-icons-png.freepik.com/512/8593/8593325.png" alt="chat icon" />
         </div>
-    `
+    `;
 
-    contentElement.className ="content-chat"
+    contentElement.className = 'content-chat';
     elementPlugin.appendChild(contentElement);
     elementPlugin.appendChild(iconElement);
-} 
+}
 
-if(elementPlugin){
+if (elementPlugin) {
     startPluginApp();
 }
